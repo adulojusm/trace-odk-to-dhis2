576be99bcb83f5b80216c0f5a849ae79a9245aee
adulojusm <43465152+adulojusm@users.noreply.github.com>
2024-04-23 10:16:14 +0100

    Delete dhis2 directory

--- dhis2/create_dhis_db_and_user.sql
+++
@@ -1,6 +0,0 @@
-create database "dhis";
-create user "dhis" with password 'dhis';
-grant all privileges on database "dhis" to "dhis";
-alter database "dhis" owner to "dhis";
-\c "dhis";
-CREATE EXTENSION postgis;
